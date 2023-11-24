839ec6d3fe7721433af7f4c09c7657735b420342
George Smirnov <sombunall@gmail.com>
2023-11-24 12:34:22 +0100

    [IMP] cx_fiq_mail_subject_task_number: remove project No from email subject

--- cx_fiq_mail_subject_task_number/models/project_task.py
+++ cx_fiq_mail_subject_task_number/models/project_task.py
@@ -7,14 +7,15 @@ class ProjectTask(models.Model):
     def _notify_get_recipients_groups(self, msg_vals=None):
         groups = super()._notify_get_recipients_groups(msg_vals)
         for group_data in groups:
-            group_data[2].update({"un_reference": self.un_reference})
+            group_data[2].update({"un_reference": self.un_reference_short})
         return groups
 
     def _message_create(self, values_list):
-        if self.un_reference:
+        if self.un_reference_short:
             values_list.update(
                 subject="{reference} {task_name}".format(
-                    reference=self.un_reference, task_name=values_list["record_name"]
+                    reference=self.un_reference_short,
+                    task_name=values_list["record_name"],
                 )
             )
         return super()._message_create(values_list)
